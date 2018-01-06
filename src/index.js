3b0a9fad746366e6aa5925857980c4d3c7ac4f10
wensan <wensansiegfred@gmail.com>
2018-01-06 22:43:19 +0800

    added purecss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import "purecss/build/pure.css";
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
